f89019e58fa95741cc51ce3b89f749f7b617c5f3
Alex <a.sandrovschii@gmail.com>
2022-06-03 14:38:38 +0200

    added tests

---
+++ utility/utility.calculations.js
@@ -0,0 +1,102 @@
+/**
+ * 
+ * @param {Number} height 
+ * @param {Number} width 
+ * @param {Number} depth 
+ * @returns 
+
+ */
+const calculateVolume = (height, width, depth) => {
+    if (checkIfPosNumber(height) && checkIfPosNumber(width) && checkIfPosNumber(depth)){
+        const result = height * depth * width;
+        return (result < 0.01 ? 0.01 : result)
+    }else{
+        return undefined
+    }
+}
+/**
+ * 
+ * @param {Number} volume 
+ * @returns 
+
+ */
+const volumePricer = (volume) => {
+    if (checkIfPosNumber(volume)){
+        if(volume < 800){
+            return 0;
+        }
+        else if(volume<12500){
+            return 0.5;
+        }
+        else if(volume>12500){
+            return 1;
+        }
+    }else{
+        return undefined
+    }
+}
+/**
+ * 
+ * @param {Number} weight 
+ * @returns 
+
+ */
+const weightPricer = (weight)=>{
+    if (checkIfPosNumber(weight)){
+        if(weight < 3){
+            return 0;
+        }
+        else if(weight<5){
+            return 0.5;
+        }
+        else if(weight>5){
+            return 1;
+        }
+    }else{
+        return undefined
+    }
+}
+/**
+ * The function calculates the amount that has to be payed for the delivery of the package based on a couple of factors.
+ * The base amount to pay is 35DKK for a delivery.
+ * @param {Number} volume Volume of the package,retrieved from a `package` object
+ * @param {Number} weight Weight of the package,retrieved from a `package` object
+ * @param {Boolean} international Will the package be internationalized,retrieved from a `delivery` object
+ * @param {Boolean} electronics Does the package contain electronics,retrieved from a `package` object
+ * @param {Boolean} oddsized Is the form of the package oddsized,retrieved from a `package` object
+ * @param {Boolean} fragile Are there any fragile things inside the package,retrieved from a `package` object
+ * @returns {Number} The price after calculations for how much to pay.
+ */
+const calculateAmount = (volume,weight,international,electronics,oddsized,fragile)=>{
+    const checkVolume = checkIfPosNumber(volume)
+    const checkWeight = checkIfPosNumber(weight)
+    const checkInternational = (typeof international == "number" && (international == 1 && international == 0))
+    const checkElectronics = (typeof electronics == "number" && (electronics == 1 && electronics == 0))
+    const checkOddsized = (typeof oddsized == "number" && (oddsized == 1 && oddsized == 0))
+    const checkFragile = (typeof fragile == "number" && (fragile == 1 && fragile == 0))
+    //35 is 35 dkk for a standard delivery price
+    if (checkVolume, checkWeight, checkInternational, checkElectronics, checkOddsized, checkFragile){
+        const baseAmountToPay= 35;
+        return baseAmountToPay+(volumePricer(volume)+weightPricer(weight)+international+electronics+oddsized+fragile)*baseAmountToPay
+    }else{
+        return undefined
+    }
+}
+
+const calculateVehicleLoad = (volume)=>{
+    //800 is m3 of volume that a vehicle can carry
+    return volume/800;
+}
+
+const checkIfPosNumber = (value) => {
+    if (typeof value == "number" && value > 0){
+        return true
+    }else{
+        return false
+    }
+}
+
+module.exports = {
+    calculateVolume,
+    calculateAmount
+}
--- utility/utility.functions.js
+++ utility/utility.functions.js
@@ -1,8 +1,8 @@
 const { execute } = require("../database/mysql.connector.js");
 
-const getDateInSqlFormat = (date) => {
-    console.log("date: ", date)
-    console.log("typeof date: ", typeof date)
+const getDateInSqlFormat = (date = null) => {
+    // console.log("date: ", date)
+    // console.log("typeof date: ", typeof date)
     if (date instanceof Date && !isNaN(date)) {
         const year = date.getFullYear()
         const month = ((date.getMonth() + 1) >= 10) ? `${date.getMonth() + 1}` : `0${date.getMonth() + 1}`
@@ -10,9 +10,9 @@ const getDateInSqlFormat = (date) => {
         const hours = (date.getHours() >= 10) ? `${date.getHours()}` : `0${date.getHours()}`
         const minutes = (date.getMinutes() >= 10) ? `${date.getMinutes()}` : `0${date.getMinutes()}`
         const seconds = (date.getSeconds() >= 10) ? `${date.getSeconds()}` : `0${date.getSeconds()}`
-        // Date format that Mysql expects to receive: YYYY-MM-DD HH:MI:SS 
+        // Date format that Mysql expects to receive: YYYY-MM-DD hh:mm:ss 
         const res = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
-        console.log("getDateInSqlFormat: ", res)
+        // console.log("getDateInSqlFormat: ", res)
         return res
     } else {
         return null
