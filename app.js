b061736a1fa140910dbec77cf6b952b1bf37fdb4
Alex <a.sandrovschii@gmail.com>
2022-06-05 20:00:31 +0200

    cleaned app

--- app.js
+++ app.js
@@ -8,11 +8,9 @@ app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
 app.use("/users", require("./src/routes/user.routes"));
-app.use("/general", require("./src/routes/general.routes"));
 app.use("/deliveries", require("./src/routes/deliveries.routes"));
 app.use("/packages", require("./src/routes/packages.routes"));
 app.use("/payments", require("./src/routes/payments.routes"));
 app.use("/locations", require("./src/routes/locations.routes"));
-app.use("/routes", require("./src/routes/routes.routes"));
 
 module.exports = app;
