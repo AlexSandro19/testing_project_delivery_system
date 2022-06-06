01d684f5ab6bb764943c6480ba7eea32d7695bf9
Alex <a.sandrovschii@gmail.com>
2022-06-06 23:08:32 +0200

    updated jest to ignore selenium

--- jest.config.js
+++ jest.config.js
@@ -90,7 +90,7 @@ module.exports = {
   // moduleNameMapper: {},
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
-  modulePathIgnorePatterns: ["jmeter", "client"],
+  modulePathIgnorePatterns: ["jmeter", "client", "selenium"],
 
   // Activates notifications for test results
   // notify: false,
