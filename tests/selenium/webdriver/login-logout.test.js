0f57a78cf9ed74534ea7ab3805dfbbbbf205ce1e
Dorin M <89907196+ddorenCorti@users.noreply.github.com>
2022-06-06 22:38:24 +0200

    login logout test

--- tests/selenium/webdriver/login-logout.test.js
+++ tests/selenium/webdriver/login-logout.test.js
@@ -1,18 +1,52 @@
+const webdriver = require('selenium-webdriver');
+const { By } = require('selenium-webdriver');
+const assert = require('assert');
+
+// async function loginlogoutOne(){
+//     let driver = await new Builder().forBrowser("firefox").build()
+//     driver.get("http://localhost:3000");
+
+//     //EMAIL
+//     var loginId = driver.findElement(By.id("email"));
+// 	loginId.sendKeys('bibendum@outlook.couk');
+//     //PASSWORD
+//     var passwd = driver.findElement(By.id("password"));
+// 	passwd.sendKeys('alex123');
+//     //SUMBIT
+//     driver.quit();
+// }
+
+async function loginLogout() {
+  let driver = new webdriver.Builder().forBrowser("firefox").build();
+  try{
+    await driver.get("http://localhost:3000/");
+
+    // EMAIL
+    const emailInput = driver.findElement(By.id("email"));
+	await emailInput.sendKeys('bibendum@outlook.couk');
+
+    const passInput = driver.findElement(By.id("password"));
+	await passInput.sendKeys('alex123');
+
+    const submitB = driver.findElement(By.css(".MuiButton-containedPrimary"));
+    await submitB.click();
+    // Thread.sleep(4000);  
+    // driver.getCurrentUrl()
+    //   .getText().then(textValue => {
+    //     assert.equal('tested string', textValue);
+    //   });
+    console.log(driver.getCurrentUrl().getText())
 
 
 
-
-
-driver.get('http://www.google.com').then(function(){
-driver.findElement(webdriver.By.name('q')).sendKeys('webdriver\n').then(function(){
-    driver.getTitle().then(function(title) {
-      console.log(title)
-      if(title === 'webdriver - Google Search') {
-         console.log('Test passed');
-      } else {
-         console.log('Test failed');
-      }
-     driver.quit();
-    });
-  });
-});
+   
+  } catch(e) {
+    //marking the test as Failed if product has not been added to the cart
+    console.log("Error:", e.message)
+    await driver.executeScript(
+      'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed","reason": "Some elements failed to load."}}'
+    );
+  }
+  await driver.quit();
+}
+loginLogout(); 
